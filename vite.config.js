d2e34f94ee154b401578803c8bbd82ef0e492ad1
Vipul Sharma <vsharmaa11116@gmail.com>
2025-07-26 10:02:25 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Add this line! This tells Vite the base public path for the deployment.
+  build: {
+    outDir: 'dist', // This is Vite's default, good to explicitly state it if you ever change it
+  },
 })
